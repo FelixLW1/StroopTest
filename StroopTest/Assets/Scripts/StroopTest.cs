d5d7ea0f2ab11eb1dd47a7f92fbd5e3fdd3aca9c
FelixL-W <felix.lambertw@gmail.com>
2021-12-18 12:17:49 +1030

    Base of the game done.

--- StroopTest/Assets/Scripts/StroopTest.cs
+++ StroopTest/Assets/Scripts/StroopTest.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+// ReSharper disable once CollectionNeverQueried.Local
 
 public class StroopTest : MonoBehaviour
 {
@@ -13,28 +18,93 @@ public class StroopTest : MonoBehaviour
         Purple
     }
 
-    private Colours answer;
+    private readonly List<Colours> _colours = new List<Colours>() {Colours.Red, Colours.Blue, Colours.Green, Colours.Purple};
+    private Colours _answer;
+    private int _score;
+    private int _answerCount;
+
+    [SerializeField] private int answersInRound;
+    
+    [Header("UI")] 
+    [SerializeField] private TextMeshProUGUI answerText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    [Header("Colours")] 
+    [SerializeField] private Color red;
+    [SerializeField] private Color blue;
+    [SerializeField] private Color green;
+    [SerializeField] private Color purple;
+    
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        _score = 0;
+        _answerCount = 0;
+        NewColour();
+    }
+
+    private void EndGame()
     {
         
     }
 
-    private void GetRandomColour()
+    public void NewColour()
     {
-        answer = Colours.Blue;
-    }
+        _answer = GetRandomColour(_colours);
 
-    public void RecieveAnswer(Colours colour)
-    {
-        if (colour == answer)
-        {
+        // copy the colours into a new list
+        var newList = new List<Colours>(_colours);
 
-            return;
-        } 
+        newList.Remove(_answer);
+
+        answerText.text = _answer.ToString();
         
-        // Give feedback
+        // Give the answer a random colour that isn't the colour of the answer
+        _answer = newList[Random.Range(0, newList.Count)];
+        answerText.color = GetColour(_answer);
     }
     
+    private Colours GetRandomColour(ICollection list)
+    {
+        return _colours[Random.Range(0, list.Count)];
+    }
+
+    private Color GetColour(Colours colour)
+    {
+        return colour switch
+        {
+            Colours.Red => red,
+            Colours.Blue => blue,
+            Colours.Green => green,
+            Colours.Purple => purple,
+            _ => throw new ArgumentOutOfRangeException(nameof(colour), colour, null)
+        };
+    }
+
+    public void RecieveAnswer(string colour)
+    {
+        print($"{colour} + {_answer.ToString()}");
+        
+        if (colour == _answer.ToString())
+        {
+            _score++;
+            scoreText.text = _score.ToString();
+        }
+
+        _answerCount++;
+
+        if (_answerCount == answersInRound)
+        {
+            EndGame(); 
+            return;
+        }
+        
+        NewColour();
+    }
 }
