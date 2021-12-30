4df2b57eefc3433886154ed7510b21e4314d6272
FelixL-W <felix.lambertw@gmail.com>
2021-12-30 12:31:16 +1030

    Added time-based score

--- StroopTest/Assets/Scripts/StroopTest.cs
+++ StroopTest/Assets/Scripts/StroopTest.cs
@@ -24,12 +24,16 @@ public class StroopTest : MonoBehaviour
     private Colours _lastAnswer;
     private int _score;
     private int _answerCount;
+    private bool _inCountdown = false;
 
-    [SerializeField] private int answersInRound;
-    
+    [Header("Game Settings")]
+    [SerializeField] private int answersInRound;  
+    [SerializeField] private int secondsToAnswer;
+
     [Header("UI")] 
     [SerializeField] private TextMeshProUGUI answerText;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI endScoreText;
     [Space] 
     [SerializeField] private Canvas menuCanvas;
     [SerializeField] private Canvas countdownCanvas;
@@ -44,30 +48,51 @@ public class StroopTest : MonoBehaviour
 
     [Space] 
     
+    [Header("References")]
     [SerializeField] private Timer timer;
-    
-    
+
+    private void OnEnable()
+    {
+        Timer.RoundTimerFinished += NewColour;
+    }
+
+    private void OnDisable()
+    {
+        Timer.RoundTimerFinished -= NewColour;
+    }
+
     // Start is called before the first frame update
     private void Start()
     {
-        menuCanvas.gameObject.SetActive(true);
-        countdownCanvas.gameObject.SetActive(false);
-        gameCanvas.gameObject.SetActive(false);
-        endCanvas.gameObject.SetActive(false);
+        GoToMainMenu();
 
         if (!timer) timer = GetComponent<Timer>();
     }
 
-    // You can't call async functions from unity buttons, so this is an in-between
+    private void Update()
+    {
+        // I know the new input system is better, but the old one is just too quick and easy not to use in this case, where there is only 1 input.
+        if (Input.GetKeyDown(KeyCode.R) && !_inCountdown) StartGame();
+    }
+
+    // You can't call async functions from unity buttons in the inspector, so this is an in-between
     public void StartGame() => StartNewGame();
     
     private async Task StartNewGame()
     {
+        timer.StopAllCoroutines();
+        
         menuCanvas.gameObject.SetActive(false);
+        gameCanvas.gameObject.SetActive(false);
         countdownCanvas.gameObject.SetActive(true);
+        endCanvas.gameObject.SetActive(false);
+
+        _inCountdown = true;
         
         // Wait for the game countdown to finish
         await timer.StartGameCountdown();
+
+        _inCountdown = false;
         
         countdownCanvas.gameObject.SetActive(false);
 
@@ -80,11 +105,20 @@ public class StroopTest : MonoBehaviour
 
     private void EndGame()
     {
-        
-    }
+        menuCanvas.gameObject.SetActive(false);
+        countdownCanvas.gameObject.SetActive(false);
+        gameCanvas.gameObject.SetActive(false);
+        endCanvas.gameObject.SetActive(true);
 
-    public void NewColour()
+        endScoreText.text = $"Score: {_score}";
+
+        _score = 0;
+    }
+    
+    private void NewColour()
     {
+        timer.StartCoroutine(timer.StartRoundCountdown(secondsToAnswer));
+        
         _answer = GetRandomColour(_colours);
 
         // copy the colours into a new list
@@ -123,21 +157,47 @@ public class StroopTest : MonoBehaviour
 
     public void RecieveAnswer(string colour)
     {
+        _answerCount++;
+        
         // If the player answers correct, add to the score
         if (colour == _answer.ToString())
         {
-            _score++;
-            scoreText.text = _score.ToString();
+            _score += 250;
+            // Add bonus score for how quickly the player guessed correctly
+            _score += (int)(timer.calculatedScore * 0.05f);
         }
+        // If they answered incorrectly, remove some points
+        else
+        {
+            _score -= 100;
 
-        _answerCount++;
-
+            if (_score < 0) _score = 0;
+        } 
+        
+        scoreText.text = "Score: " + _score.ToString();
+        
         if (_answerCount == answersInRound)
         {
             EndGame(); 
             return;
         }
         
+        // Stop the last colour timer
+        timer.StopAllCoroutines();
+        
         NewColour();
+    }
+
+    public void GoToMainMenu()
+    {
+        menuCanvas.gameObject.SetActive(true);
+        gameCanvas.gameObject.SetActive(false);
+        countdownCanvas.gameObject.SetActive(false);
+        endCanvas.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- StroopTest/Assets/Scripts/Timer.cs
+++ StroopTest/Assets/Scripts/Timer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,6 +10,11 @@ public class Timer : MonoBehaviour
     [SerializeField] private TextMeshProUGUI gameTimerText;
     [SerializeField] private TextMeshProUGUI roundTimerText;
 
+    [HideInInspector] public float calculatedScore;
+
+    public static event Action RoundTimerFinished;
+
+    // The 3, 2, 1 Countdown when the game starts
     public async Task StartGameCountdown(int waitTime = 3)
     {
         gameTimerText.text = waitTime.ToString();
@@ -20,6 +26,38 @@ public class Timer : MonoBehaviour
             waitTime--;
             
             gameTimerText.text = waitTime.ToString();
+        }
+    }
+
+    // The countdown for each answer in a round
+    public IEnumerator StartRoundCountdown(int seconds)
+    {
+        StartCoroutine(CountdownScore(seconds));
+        
+        roundTimerText.text = seconds.ToString();
+
+        while (seconds > 0)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+
+            seconds --;
+
+            roundTimerText.text = seconds.ToString();
+        }
+        
+        RoundTimerFinished?.Invoke();
+    }
+
+    // Counts down each millisecond to give a bonus score
+    private IEnumerator CountdownScore(int seconds)
+    {
+        calculatedScore = seconds * 1000;
+
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            calculatedScore -= Time.deltaTime * 1000;
         }
     }
 }
